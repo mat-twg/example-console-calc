f941fc92b7fc52321d9c03a11ac554a9ed4bd440
Matvey Alekseev <mat@twg.su>
2023-09-06 11:35:15 +0300

    Refactor calc
    - remove eval
    - add malformed check
    - tests update

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -28,17 +28,74 @@ describe('Check', () => {
   });
 });
 
+describe('Malformed expression', () => {
+  it(`Неправильное выражение: '1+'`, () => {
+    expect(main.checkMalformed('1+')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '1-'`, () => {
+    expect(main.checkMalformed('1-')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '/1'`, () => {
+    expect(main.checkMalformed('/1')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '*1'`, () => {
+    expect(main.checkMalformed('*1')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '1*'`, () => {
+    expect(main.checkMalformed('1*')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '1/'`, () => {
+    expect(main.checkMalformed('1/')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '1/*1'`, () => {
+    expect(main.checkMalformed('1/*1')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+  it(`Неправильное выражение: '1*/1'`, () => {
+    expect(main.checkMalformed('1*/1')).rejects.toBe(
+      main.CALC.ERROR + main.CALC.ERROR_MALFORMED,
+    );
+  });
+});
+
 describe('Calc', () => {
   it(`Подготовка выражения: (1+2)(2-1)/3(3*3)'`, () => {
     expect(main.prepare('(1+2)(2-1)/3(3*3)')).resolves.toBe(
       '(1+2)*(2-1)/3*(3*3)',
     );
   });
+  it('Деление на ноль', () => {
+    expect(main.calc('1/0')).rejects.toBe(main.CALC.DIVISION_BY_ZERO);
+  });
   it(`Расчёт: '(1+2)*(2-1)/3*(3*3)'`, () => {
     expect(main.calc('(1+2)*(2-1)/3*(3*3)')).resolves.toBe(9);
   });
-  it('Деление на ноль', () => {
-    expect(main.calc('1/0')).rejects.toBe(main.CALC.DIVISION_BY_ZERO);
+  it(`Расчёт: '2*((3*(-1.24*+4+5*6-7/-2.66-2/-1+7+8/9*2+3))*(22.11+1)-(2/22.22)/-(2-1))'`, () => {
+    expect(
+      main.calc(
+        '2*((3*(-1.24*+4+5*6-7/-2.66-2/-1+7+8/9*2+3))*(22.11+1)-(2/22.22)/-(2-1))',
+      ),
+    ).resolves.toBe(5747.547821510571);
+  });
+  it(`Расчёт: '2((3(-1.24*+4+5*6-7/-2.66-2/-1+7+8/9*2+3))(22.11+1)-(2/22.22)/-(2-3))'`, () => {
+    expect(
+      main.calc(
+        '2*((3*(-1.24*+4+5*6-7/-2.66-2/-1+7+8/9*2+3))*(22.11+1)-(2/22.22)/-(2-3))',
+      ),
+    ).resolves.toBe(5747.1877855069715);
   });
 });
 
@@ -51,8 +108,8 @@ describe('Output', () => {
   });
   it('Проверка вывода ошибки', async () => {
     const logSpy = jest.spyOn(console, 'error').mockImplementation();
-    await main.parse('()');
-    expect(logSpy).toHaveBeenCalledWith(main.CALC.ERROR + main.CALC.ERROR_MATH);
+    await main.parse('1/0');
+    expect(logSpy).toHaveBeenCalledWith(main.CALC.DIVISION_BY_ZERO);
     logSpy.mockClear();
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,16 @@ export enum CALC {
   ERROR_CHARS = 'допускается использовать только символы 0-9,.+*/()',
   ERROR_DOUBLES = 'найдено недопустимое повторение символов',
   ERROR_BRACKETS = 'неверно указаны скобки',
-  ERROR_MATH = 'невозможно рассчитать',
+  ERROR_MALFORMED = 'Неправильное выражение',
   DIVISION_BY_ZERO = 'Деление на ноль запрещено!',
   RESULT = 'Результат: ',
+}
+
+export enum OPERATOR {
+  PLUS = '+',
+  MINUS = '-',
+  DIV = '/',
+  MULT = '*',
 }
 
 export const checkChars = (expr: string): Promise<string> => {
@@ -26,6 +33,16 @@ export const checkChars = (expr: string): Promise<string> => {
     return /^([.+\-*\/()\d])+$/gs.test(string)
       ? resolve(string)
       : reject(CALC.ERROR + CALC.ERROR_CHARS);
+  });
+};
+
+export const checkMalformed = (expr: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    return /[+-\/*]$/g.test(expr) ||
+      /\*\/|\/\*/g.test(expr) ||
+      /^\/|^\*/g.test(expr)
+      ? reject(CALC.ERROR + CALC.ERROR_MALFORMED)
+      : resolve(expr);
   });
 };
 
@@ -76,19 +93,125 @@ export const prepare = (expr: string): Promise<string> => {
 };
 
 export const calc = (expr: string): Promise<number> => {
+  const sequenceCalc = (input: string): number => {
+    input = input.replace(/--/g, '+');
+
+    let result = 0;
+    let temp = '';
+
+    const process = (input: string) => {
+      if (input.length === 0) {
+        return;
+      }
+
+      if (!/^[+-\/*]/g.test(input)) {
+        result = parseFloat(input);
+        return;
+      }
+
+      const operator = input.charAt(0);
+      input = input.slice(1);
+
+      switch (operator) {
+        case OPERATOR.PLUS:
+          return (result += parseFloat(input));
+        case OPERATOR.MINUS:
+          return (result -= parseFloat(input));
+        case OPERATOR.MULT:
+          return (result *= parseFloat(input));
+        case OPERATOR.DIV:
+          const value = parseFloat(input);
+          if (value === 0) {
+            throw new Error(CALC.DIVISION_BY_ZERO);
+          }
+          result /= value;
+      }
+    };
+
+    Array.from(input).forEach((char) => {
+      if (Object.values(OPERATOR).includes(char as OPERATOR)) {
+        process(temp);
+        temp = '';
+      }
+      temp += char;
+    });
+    process(temp);
+
+    return result;
+  };
+
+  const mergeResults = (
+    input: string,
+    results: Record<string, number>,
+  ): string => {
+    let output = input;
+    Object.keys(results)?.forEach(
+      (key) => (output = output.replace(key, results[key].toString())),
+    );
+    return output;
+  };
+
+  const collapseMultDiv = (input: string): string => {
+    const results: Record<string, number> = {};
+
+    input = input.replace(/--/g, '+');
+
+    if (!/[+-]/g.test(input) && input.match(/[\/*]/g).length > 1) {
+      results[input] = sequenceCalc(input);
+
+      return mergeResults(input, results);
+    }
+
+    input.match(/[\d.]+[*\/][+\-]*[\d.]+/g)?.forEach((node) => {
+      if (node.indexOf(OPERATOR.MULT) > 0) {
+        const temp = node.split(OPERATOR.MULT);
+        results[node] = parseFloat(temp[0]) * parseFloat(temp[1]);
+      }
+      if (node.indexOf(OPERATOR.DIV) > 0) {
+        const temp = node.split(OPERATOR.DIV);
+        if (parseFloat(temp[1]) === 0) {
+          throw new Error(CALC.DIVISION_BY_ZERO);
+        }
+        results[node] = parseFloat(temp[0]) / parseFloat(temp[1]);
+      }
+    });
+    const output = mergeResults(input, results);
+
+    return /[\/*]/g.test(output) ? collapseMultDiv(output) : output;
+  };
+
+  const collapseAll = (input: string): string => {
+    input = input.replace(/--/g, '+');
+
+    const matches = input.match(/\([^()]*\)/g).map((result) => result);
+    const results = {};
+
+    for (const node of matches) {
+      results[node] = [];
+      const preparedNode = node.replace(/[()]/g, '');
+      const resultMultDiv = collapseMultDiv(preparedNode);
+
+      results[node] = sequenceCalc(resultMultDiv);
+    }
+    const output = mergeResults(input, results);
+
+    return /[\/*]/g.test(output) ? collapseAll(output) : output;
+  };
+
   return new Promise((resolve, reject) => {
     try {
-      const result = eval(`(${expr})`);
+      const result = collapseAll(`(${expr})`).replace(/[()]/g, '');
 
-      return isFinite(result) ? resolve(result) : reject(CALC.DIVISION_BY_ZERO);
+      return resolve(+result);
     } catch (e) {
-      reject(CALC.ERROR + CALC.ERROR_MATH);
+      reject(e.message);
     }
   });
 };
 
 export const parse = async (expr: string): Promise<void> => {
   return Promise.resolve(checkChars(expr))
+    .then(checkMalformed)
     .then(checkDoubles)
     .then(checkBrackets)
     .then(prepare)
